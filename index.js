a1e30d5e9546bdbf6525914c15e4f097b340b4c5
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-03-11 15:54:17 +0000

    M1D10 homework incomplete

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 /*
 JS EXERCISES
 
-21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
-22) Create an object with properties such name, surname, email
-23) Delete the email property from the previously created object
-24) Create an array with 10 strings in it
-25) Print in the console every string from the previous array
-26) Create an array with 100 random numbers in it
+
 27) Write a function to get the maximum and minimum values from the previously created array
 28) Create an array of arrays, in which every array has 10 random numbers
 29) Create a function that gets 2 arrays as parameters and returns the longest one
 30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+*/
+// 21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
+let x = "John";
+let y = "Doe";
+
+
+console.log(x + " <> " + y)
+
+//22) Create an object with properties such name, surname, email
+
+const person = { name: "raeven", surname: "goncalves", email: "raevengaoncalves@gamil.com" };
+
+// 23) Delete the email property from the previously created object
+
+delete person.email;
+
+//24) Create an array with 10 strings in it
+
+let strings = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+
+//25) Print in the console every string from the previous array
+
+console.log(strings)
+
+//25) Print in the console every string from the previous array
+
+for (let i = 0; i < strings.length; i++) {
+    console.log(strings[i]);
+}
+
+//26) Create an array with 100 random numbers in it
+let random = [Math.floor(Math.random() * 100) + 1];
+
+for (let i = 0; i < random.length; i++) {
+    console.log(random[i]);
+}
+
+
+
+/*
 
 DOM EXERCISES
 
-31) Get the element with an id of "container" from the page
-32) Get every <td> element from the page
-33) Use a loop for printing the text inside of every <td> element in the page
+
+
+
 34) Write a function to change the heading of the page
 35) Write a function to add an extra row to the table
 36) Write a function to add a class of "test" to each row in the table
@@ -24,7 +59,20 @@ DOM EXERCISES
 38) Console log "Page loaded" when the page is correctly loaded
 39) Write a function to add new items to a unordered list
 40) Write a function to empty a list
+*/
 
+//31) Get the element with an id of "container" from the page
+
+let node = document.getElementById(header)
+
+//32) Get every <td> element from the page
+
+let elemnent = document.getElementsByTagName("tr")
+
+
+//33) Use a loop for printing the text inside of every <td> element in the page
+
+/*
 EXTRA EXERCISES
 
 41) Add an eventListener to show an alert when the cursor hovers a link, displaying its href property
